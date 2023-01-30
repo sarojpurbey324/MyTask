153ab7e4b8eb434ece4e9b651507633cb3cc0cf8
Saroj Purbey <sarojpurbey324@gmail.com>
2023-01-30 21:54:09 +0545

    Log added in controller and service layer

--- src/main/java/com/clusus/task/controller/DealDataController.java
+++ src/main/java/com/clusus/task/controller/DealDataController.java
@@ -2,26 +2,29 @@ package com.clusus.task.controller;
 
 import com.clusus.task.model.dto.DealRequestDTO;
 import com.clusus.task.service.DealService;
+import com.clusus.task.service.DealServiceImpl;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("deal-data")
 public class DealDataController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DealServiceImpl.class);
 
     @Autowired
     private DealService dealService;
 
     @PostMapping("/save-data")
     public ResponseEntity<?> saveDealData(@Valid @RequestBody DealRequestDTO dealDataDTO) {
+        LOGGER.info("Receiving request at endpoint /save-data");
         dealService.saveDealData(dealDataDTO);
         return ResponseEntity.ok().body("Deal Data saved successfully.");
 
@@ -29,6 +32,7 @@ public class DealDataController {
 
     @PostMapping("/save-all-data")
     public ResponseEntity<?> saveDealData(@Valid @RequestBody List<DealRequestDTO> dealDataDTO) {
+        LOGGER.info("Receiving request at endpoint /save-all-data");
         dealService.saveAllDealData(dealDataDTO);
         return ResponseEntity.ok().body("Deal Data saved successfully.");
     }
--- src/main/java/com/clusus/task/service/DealServiceImpl.java
+++ src/main/java/com/clusus/task/service/DealServiceImpl.java
@@ -4,6 +4,8 @@ import com.clusus.task.model.dto.DealRequestDTO;
 import com.clusus.task.model.entity.DealDataEntity;
 import com.clusus.task.repository.DealRepository;
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,19 +15,22 @@ import java.util.List;
 @Service
 public class DealServiceImpl implements DealService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DealServiceImpl.class);
+
     @Autowired
     private DealRepository dealRepository;
 
     @Override
     @Transactional
     public void saveDealData(DealRequestDTO dealRequestDTO) {
+        LOGGER.info("Saving single deal data");
         DealDataEntity dealDataEntity = new DealDataEntity();
         dealDataEntity.setDealUniqueId(dealRequestDTO.getDealId());
         dealDataEntity.setFromCurrencyISOCode(dealRequestDTO.getFromCurrencyIsoCode());
         dealDataEntity.setToCurrencyISOCode(dealRequestDTO.getToCurrencyIsoCode());
         dealDataEntity.setDealTimestamp(dealRequestDTO.getDealTimestamp());
         dealDataEntity.setDealAmount(dealRequestDTO.getDealAmount());
-        if(dealRepository.existsByDealUniqueId(dealRequestDTO.getDealId())){
+        if (dealRepository.existsByDealUniqueId(dealRequestDTO.getDealId())) {
             throw new RuntimeException(" Data with this deal id already exists");
         } else {
             dealRepository.save(dealDataEntity);
@@ -35,8 +40,9 @@ public class DealServiceImpl implements DealService {
     @Override
     @Transactional(rollbackOn = {})
     public void saveAllDealData(List<DealRequestDTO> dealRequestDataList) {
+        LOGGER.info("Saving list of deal data");
         List<DealDataEntity> dealDataList = new ArrayList<>();
-        for(DealRequestDTO dealRequestData: dealRequestDataList){
+        for (DealRequestDTO dealRequestData : dealRequestDataList) {
             DealDataEntity dealDataEntity = new DealDataEntity();
             dealDataEntity.setDealUniqueId(dealRequestData.getDealId());
             dealDataEntity.setFromCurrencyISOCode(dealRequestData.getFromCurrencyIsoCode());
